0b1b8abcd16eef4db2608dc62d2b3fd1a019570a
robren <rob@robren.net>
2017-01-30 12:06:21 -0500

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='sec_edgar_download',
-    version='0.0.1',
+    version='0.0.2',
     description="Downloads sec xbrl filings",
     long_description=readme + '\n\n' + history,
     author="Robert Rennison",
