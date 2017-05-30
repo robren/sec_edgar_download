ecc935de51f83723f5122f5a4ba2934b444c4ffe
robren <rob@robren.net>
2017-05-30 15:52:47 -0400

    Bump version: 0.1.1 → 0.1.2

--- sec_edgar_download/__init__.py
+++ sec_edgar_download/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Robert Rennison"""
 __email__ = 'rob@robren.net'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='sec_edgar_download',
-    version='0.1.1',
+    version='0.1.2',
     description="Downloads sec xbrl filings",
     long_description=readme + '\n\n' + history,
     author="Robert Rennison",
